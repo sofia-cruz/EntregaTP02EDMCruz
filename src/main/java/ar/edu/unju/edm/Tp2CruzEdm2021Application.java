d891d127b9f00990063d7e3c54749a4a21c20a0b
ASROCK RX580 2020 <ASROCK RX580 2020@DESKTOP-0JPMBL8>
2021-04-16 15:37:46 -0300

    Inicialización de Proyecto
    Cruz, Sofía Candela

---
+++ src/main/java/ar/edu/unju/edm/Tp2CruzEdm2021Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2CruzEdm2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2CruzEdm2021Application.class, args);
+	}
+
+}
